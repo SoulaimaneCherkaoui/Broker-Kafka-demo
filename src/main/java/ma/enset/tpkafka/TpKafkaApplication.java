14682d8f56dbab72613ccd97fd3350220a53045b
SoulaimaneCherkaoui <soulaimanecherkaoui0@gmail.com>
2024-10-07 20:05:11 +0100

    first commit

---
+++ src/main/java/ma/enset/tpkafka/TpKafkaApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.tpkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/tpkafka/TpKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.tpkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
